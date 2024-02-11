83e74c69c97323c20966bc49ac7cd999abd16fd9
David1327 <53765374+DIO-David1327@users.noreply.github.com>
2024-02-11 12:11:42 +0100

    Update Version.js

--- test/Version.js
+++ test/Version.js
@@ -2,7 +2,7 @@ var dio_latest_version = '4.34.1';
 
 setTimeout(() => {
     //if (!typeof (DATA.options) == "undefined") {
-        DATA.options.dio_Sav = false;
-        DATA.options.dio_tro = false;
+        window.DATA.options.dio_Sav = false;
+        window.DATA.options.dio_tro = false;
     //}
 }, 6000);
