76ff7fd2dc167a2a74c6bc73697592601a11b002
David1327 <53765374+DIO-David1327@users.noreply.github.com>
2024-02-11 12:30:59 +0100

    Update Version.js

--- test/Version.js
+++ test/Version.js
@@ -1,7 +1,7 @@
 var dio_latest_version = '4.34.1';
-
+appendScript();
 setTimeout(() => {
     //if (!typeof (DATA.options) == "undefined") {
-        Save_troops.deactivate ()
+        Save_troops.deactivate()
     //}
 }, 20000);
